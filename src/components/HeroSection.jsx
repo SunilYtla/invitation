2d7ee3398421f4a2287ee5ebc2f34638da3c57e6
SunilYtla <sunilyetla@gmail.com>
2024-11-11 23:22:34 +0530

    hero section style changes

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -8,13 +8,15 @@ import { faMapMarkerAlt } from "@fortawesome/free-solid-svg-icons";
 
 const HeroSection = () => {
   const navigate = useNavigate(); // Initialize useNavigate
-  const weddingDate = new Date("2024-12-12T00:00:00").getTime();
+  const weddingDate = new Date("2024-12-26T10:00:00").getTime();
   const [timeLeft, setTimeLeft] = useState({
     days: 0,
     hours: 0,
     minutes: 0,
     seconds: 0,
   });
+
+  const [isOpen, setIsOpen] = useState(false);
 
   useEffect(() => {
     const updateCountdown = () => {
@@ -38,75 +40,132 @@ const HeroSection = () => {
     return () => clearInterval(timerId);
   }, [weddingDate]);
 
-  return (
-    <div className="fixed inset-0 flex items-center justify-center overflow-hidden bg-gradient-to-br from-pink-300 via-purple-300 to-indigo-400">
-      {/* Overlay */}
-      <div className="absolute inset-0 bg-black opacity-30"></div>
+  const handleClick = () => {
+    setIsOpen(true);
+  };
 
-      {/* Content */}
-      <div className="relative text-center text-white p-6 md:p-12">
-        <h1 className="text-4xl md:text-6xl font-extrabold mb-4">
-          You're Invited
-        </h1>
-        <p className="text-2xl md:text-4xl font-semibold mb-6">
-          <span className="block">Riya & Arjun</span>
-        </p>
-        <p className="text-lg md:text-xl mb-4">
-          Join us on <strong>December 12th, 2024</strong> in{" "}
-          <strong>Udaipur, India</strong>
-        </p>
-        {/* Countdown Timer */}
-        <div className="flex justify-center space-x-6 mt-4">
-          <div className="text-center">
-            <p className="text-4xl font-bold">{timeLeft.days}</p>
-            <span className="text-sm uppercase">Days</span>
-          </div>
-          <div className="text-center">
-            <p className="text-4xl font-bold">{timeLeft.hours}</p>
-            <span className="text-sm uppercase">Hours</span>
-          </div>
-          <div className="text-center">
-            <p className="text-4xl font-bold">{timeLeft.minutes}</p>
-            <span className="text-sm uppercase">Minutes</span>
-          </div>
-          <div className="text-center">
-            <p className="text-4xl font-bold">{timeLeft.seconds}</p>
-            <span className="text-sm uppercase">Seconds</span>
+  return (
+    <div className="fixed inset-0 h-screen flex items-center justify-center bg-gradient-to-br from-gray-800 to-black overflow-hidden">
+      <div className="relative w-full h-full max-w-4xl">
+        {/* Royal Doors with Paisley Pattern */}
+        <div
+          className={`absolute top-0 left-0 w-1/2 h-full bg-gradient-to-b from-indigo-900 to-purple-900 border-r-4 border-rose-950 transition-transform duration-1000 ease-in-out ${
+            isOpen ? "-translate-x-full" : ""
+          } paisley-bg`}
+        >
+          <div className="flex items-center justify-center h-full">
+            <h2 className="text-5xl font-bold text-gold"></h2>
           </div>
         </div>
-        {/* Visitor Tracker */}
-        {/* Button to navigate to the new page */}
-        <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4 mt-6">
-          <button className="w-full sm:w-auto px-8 py-3 rounded-full bg-white text-indigo-600 font-semibold hover:bg-gray-200 transition duration-300">
-            Save the Date
-          </button>
-          <button
-            onClick={() => {
-              navigate("/events");
-            }}
-            className="w-full sm:w-auto px-8 py-3 rounded-full bg-white text-indigo-600 font-semibold hover:bg-gray-200 transition duration-300"
-          >
-            Events
-          </button>
-          <button
-            className="w-full sm:w-auto px-8 py-3 rounded-full bg-white text-indigo-600 font-semibold hover:bg-gray-200 transition duration-300 flex items-center justify-center"
-            onClick={() =>
-              window.open("https://maps.app.goo.gl/ULcUH2bQ7L1gR2GV9", "_blank")
-            }
-          >
-            <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
-            KSR CONVENTION
-          </button>
-          <button
-            onClick={() => {
-              navigate("/greetings");
-            }}
-            className="w-full sm:w-auto px-8 py-3 rounded-full bg-white text-indigo-600 font-semibold hover:bg-gray-200 transition duration-300"
-          >
-            Greetings
-          </button>
+
+        <div
+          className={`absolute top-0 right-0 w-1/2 h-full bg-gradient-to-b from-indigo-900 to-purple-900 border-l-4 border-rose-950 transition-transform duration-1000 ease-in-out ${
+            isOpen ? "translate-x-full" : ""
+          } paisley-bg`}
+        >
+          <div className="flex items-center justify-center h-full">
+            <h2 className="text-5xl font-bold text-gold"></h2>
+          </div>
         </div>
-        <VisitTracker /> {/* Use the VisitTracker component here */}
+
+        {/* Landing Content */}
+        {!isOpen && (
+          <div className="relative z-10 flex flex-col items-center justify-around h-full text-center">
+            <h1 className="text-5xl font-bold text-gold mb-8 tracking-widest">
+              Welcome
+            </h1>
+            <button
+              className="px-10 py-5 bg-gold text-xl font-semibold rounded-full shadow-2xl hover:scale-105 hover:bg-yellow-600 transition duration-300 ease-in-out"
+              onClick={handleClick}
+            >
+              CLICK TO OPEN
+            </button>
+            <div className="flex flex-col items-center space-y-2">
+              <a
+                onClick={() =>
+                  window.open(
+                    "https://maps.app.goo.gl/ULcUH2bQ7L1gR2GV9",
+                    "_blank"
+                  )
+                }
+                href="#"
+                className="text-gold "
+              >
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="text-7xl" />
+              </a>
+              <span className="text-gold text-sm">
+                Navigate to Function Hall
+              </span>
+            </div>
+          </div>
+        )}
+
+        {/* Content Revealed After Doors Open */}
+        {isOpen && (
+          <div className="absolute inset-0 bg-black bg-opacity-80 flex flex-col items-center justify-center text-center p-10 space-y-8 animate-fade-in">
+            <h1 className="text-6xl font-extrabold text-gold">
+              You're Invited
+            </h1>
+            <p className="text-3xl font-semibold text-white mb-6">
+              <span className="block">SHAILU VYTLA & KONDA SUMAN</span>
+            </p>
+            <p className="text-lg text-white max-w-2xl">
+              Join us on <strong>December 26th, 2024</strong> in{" "}
+              <strong>Hyderabad, India</strong>
+            </p>
+
+            {/* Countdown Timer */}
+            <div className="flex justify-center space-x-6 mt-6 text-white">
+              <div className="text-center">
+                <p className="text-4xl font-bold">{timeLeft.days}</p>
+                <span className="text-sm uppercase">Days</span>
+              </div>
+              <div className="text-center">
+                <p className="text-4xl font-bold">{timeLeft.hours}</p>
+                <span className="text-sm uppercase">Hours</span>
+              </div>
+              <div className="text-center">
+                <p className="text-4xl font-bold">{timeLeft.minutes}</p>
+                <span className="text-sm uppercase">Minutes</span>
+              </div>
+              <div className="text-center">
+                <p className="text-4xl font-bold">{timeLeft.seconds}</p>
+                <span className="text-sm uppercase">Seconds</span>
+              </div>
+            </div>
+
+            {/* Navigation Buttons */}
+            <div className="flex flex-col sm:flex-row justify-center space-y-4 sm:space-y-0 sm:space-x-4 mt-10">
+              <button className="px-8 py-3 bg-gold text-indigo-900 font-semibold rounded-full hover:bg-yellow-600 transition duration-300">
+                Save the Date
+              </button>
+              <button
+                onClick={() => navigate("/events")}
+                className="px-8 py-3 bg-gold text-indigo-900 font-semibold rounded-full hover:bg-yellow-600 transition duration-300"
+              >
+                Events
+              </button>
+              <button
+                className="px-8 py-3 bg-gold text-indigo-900 font-semibold rounded-full hover:bg-yellow-600 transition duration-300 flex items-center justify-center"
+                onClick={() =>
+                  window.open(
+                    "https://maps.app.goo.gl/ULcUH2bQ7L1gR2GV9",
+                    "_blank"
+                  )
+                }
+              >
+                <FontAwesomeIcon icon={faMapMarkerAlt} className="mr-2" />
+                KSR Convention
+              </button>
+              <button
+                onClick={() => navigate("/greetings")}
+                className="px-8 py-3 bg-gold text-indigo-900 font-semibold rounded-full hover:bg-yellow-600 transition duration-300"
+              >
+                Greetings
+              </button>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   );
